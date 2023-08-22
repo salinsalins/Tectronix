d745f03f98f6f39885247cc4d55933735cf60706
salinsalins <sanin_andrey@mail.ru>
2023-08-22 17:23:10 +0700

    start tec. 3014

--- Tectronix.py
+++ Tectronix.py
@@ -9,7 +9,18 @@ tec_ip = "192.168.1.222"
 
 # params = urllib.parse.urlencode({'COMMAND': '*idn?', '@gpibsend': 'Send', '@name': ''})
 # command = '*idn?'
-command = 'CH1:COUP DC'
+# command = 'CH1:COUP DC' # set channel 1 coupling to DC (AC, GND)
+# command = 'CH1:IMP MEG' # set channel 1 impedance to 1MOhm (FIF)
+# command = 'CH1:VOL 1.0' # set channel 1 impedance to 1MOhm (FIF)
+# command = 'CH1:POS 2.0' # set channel 1 impedance to 1MOhm (FIF)
+command = 'HOR:SCAL 0.1'    # sampling 0.1 s|div
+command = 'HOR:TRIG:POS 50.0'    # horiz. trigger position % of screen
+command = 'CH1:POS 1.0'     # vertical position at + 1.0 division
+command = 'ID?'             # similar to *idn?
+command = '*LRN?'           # read all settings
+command = 'TRIG?'           # query trigger params
+command = 'TRIG FORC'       # force trigger
+command = 'TRIG:STATE?'     # trigger state
 params = ('COMMAND=' + command + '\n\rgpibsend=Send\n\rname=\n\r').encode()
 headers = {"Content-type": "text/plain",
            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
@@ -17,11 +28,13 @@ conn = http.client.HTTPConnection(tec_ip)
 conn.request("POST", "/Comm.html", params, headers)
 response = conn.getresponse()
 print(response.status, response.reason)
-data = response.read(1300)
-# while True:
-#     data = response.read(1300)
-#     if not data:
-#         break
+
+data = b''
+while True:
+    get = response.read(1024)
+    if not get:
+        break
+    data += get
 print(data)
 d_data = data.decode()
 n = d_data.find('<TEXTAREA')
