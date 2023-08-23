6bec66f32d46adb5ae2b6ac40caf75c551d6809c
salinsalins <sanin_andrey@mail.ru>
2023-08-23 12:10:08 +0700

    start tec. 3014

--- Tectronix.py
+++ Tectronix.py
@@ -1,6 +1,11 @@
 import http.client, urllib.parse
 import io
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from isfread import isfread
+
 
 tec_ip = "192.168.1.222"
 
@@ -15,21 +20,22 @@ tec_ip = "192.168.1.222"
 # command = 'CH1:IMP MEG' # set channel 1 impedance to 1MOhm (FIF)
 # command = 'CH1:VOL 1.0' # set channel 1 impedance to 1MOhm (FIF)
 # command = 'CH1:POS 2.0' # set channel 1 impedance to 1MOhm (FIF)
-command = 'HOR:SCAL 0.1'  # sampling 0.1 s|div
-command = 'HOR:TRIG:POS 50.0'  # horiz. trigger position % of screen
-command = 'CH1:POS 1.0'  # vertical position at + 1.0 division
-command = 'ID?'  # similar to *idn?
-command = '*LRN?'  # read all settings
-command = 'TRIG?'  # query trigger params
-command = 'TRIG FORC'  # force trigger
-command = 'TRIG:STATE?'  # trigger state
+# command = 'HOR:SCAL 0.1'  # sampling 0.1 s|div
+# command = 'HOR:TRIG:POS 50.0'  # horiz. trigger position % of screen
+# command = 'CH1:POS 1.0'  # vertical position at + 1.0 division
+# command = 'ID?'  # similar to *idn?
+# command = '*LRN?'  # read all settings
+# command = 'TRIG?'  # query trigger params
+# command = 'TRIG FORC'  # force trigger
+# command = 'TRIG:STATE?'  # trigger state
 
 
-def connect(ip):
+def tec_connect(ip):
     conn = http.client.HTTPConnection(ip)
     return conn
 
-def send_command(conn, cmd):
+
+def tec_send_command(conn, cmd):
     params = ('COMMAND=' + cmd + '\n\rgpibsend=Send\n\rname=\n\r').encode()
     headers = {"Content-type": "text/plain",
                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
@@ -53,10 +59,10 @@ def send_command(conn, cmd):
     m = data.find('</TEXTAREA>')
     if m < 0:
         return None
-    return data[n+n1+1:m]
+    return data[n + n1 + 1:m]
 
 
-def get_image(conn):
+def tec_get_image(conn):
     params = b''
     headers = {"Accept": "image/avif,image/webp,*/*"}
     conn.request("GET", "/Image.png", params, headers)
@@ -70,8 +76,10 @@ def get_image(conn):
     img = Image.open(io.BytesIO(data))
     return img
 
-def get_data(chan_number):
-    params = b'command=select:ch3 on\r\ncommand=save:waveform:fileformat internal\r\nwfmsend=Get\r\n'
+
+def tec_get_data(conn, chan_number):
+    str = 'command=select:ch%s on\r\ncommand=save:waveform:fileformat internal\r\nwfmsend=Get\r\n' % chan_number
+    params = str.encode()
     headers = {"Accept": "text/html, application/xhtml+xml, image/jxr, */*",
                "Content-Type": "text/plain",
                "Cache-Control": "no-cache"}
@@ -88,75 +96,26 @@ def get_data(chan_number):
 
 
 t0 = time.time()
-params = ('COMMAND=' + command + '\n\rgpibsend=Send\n\rname=\n\r').encode()
-headers = {"Content-type": "text/plain",
-           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
-conn = http.client.HTTPConnection(tec_ip)
-conn.request("POST", "/Comm.html", params, headers)
-response = conn.getresponse()
-print(response.status, response.reason)
-
-data = b''
-while True:
-    d = response.read(1024)
-    if not d:
-        break
-    data += d
-print('Elapsed', time.time()-t0, 's')
-print(data)
-d_data = data.decode()
-n = d_data.find('<TEXTAREA')
-n1 = d_data[n:].find('>')
-m = d_data.find('</TEXTAREA>')
-print(d_data[n + n1 + 1:m])
-print('Elapsed', time.time()-t0, 's')
+conn = tec_connect(tec_ip)
+print('Elapsed', time.time() - t0, 's')
 
 t0 = time.time()
-params = b''
-headers = {"Accept": "image/avif,image/webp,*/*"}
-conn.request("GET", "/Image.png", params, headers)
-response = conn.getresponse()
-data = b''
-while True:
-    d = response.read(1024)
-    if not d:
-        break
-    data += d
-print('Elapsed', time.time()-t0, 's', len(data))
-# print(repr(data))
+result = tec_send_command(conn, 'TRIG:STATE?')
+print('Elapsed', time.time() - t0, 's')
+print(result)
 
-import matplotlib.pyplot as plt
-import numpy as np
-from PIL import Image
-
-img = Image.open(io.BytesIO(data))
-# print(repr(img))
-imgplot = plt.imshow(img)
+t0 = time.time()
+result = tec_get_image(conn)
+print('Elapsed', time.time() - t0, 's')
+imgplot = plt.imshow(result)
 plt.show()
 
 t0 = time.time()
-params = b'command=select:ch3 on\r\ncommand=save:waveform:fileformat internal\r\nwfmsend=Get\r\n'
-headers = {"Accept": "text/html, application/xhtml+xml, image/jxr, */*",
-           "Content-Type": "text/plain",
-           "Cache-Control": "no-cache"}
-conn.request("POST", "/getwfm.isf", params, headers)
-response = conn.getresponse()
-data = b''
-while True:
-    d = response.read(1024)
-    if not d:
-        break
-    data += d
-print('Elapsed', time.time()-t0, 's', len(data))
-
-from isfread import isfread
-
-x, y, head = isfread(io.BytesIO(data))
-
+x, y, head = tec_get_data(conn, 2)
+print('Elapsed', time.time() - t0, 's')
 plt.plot(x, y)
 plt.xlabel('Time (ms)')
 plt.ylabel('Voltage (mV)')
-# plt.legend([str(i) for i in pl.channels])
 plt.show()
 
 conn.close()
